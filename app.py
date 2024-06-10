b30ff1575a18ea85e3c24237e3e2cef77ddb53d2
Liamhewitt05 <liamhewitt1311@gmail.com>
2024-06-10 23:44:09 +0200

    AI role config

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, url_for
 from dotenv import load_dotenv
 import os
 from openai import OpenAI
-
+from service_config import service_config
 load_dotenv()
 
 app = Flask(__name__)
@@ -12,10 +12,10 @@ open_ai_client = OpenAI(
     api_key=OPENAI_KEY
 )
 
-def query_open_ai(prompt, outputs=3): 
+def query_open_ai(prompt, ai_role, outputs=3): 
     completion = open_ai_client.chat.completions.create(
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": ai_role},
             {"role": "user", "content": prompt}
         ],
         model="gpt-3.5-turbo",
@@ -49,12 +49,12 @@ def service_list():
 @app.route('/service/<name>', methods=('GET', 'POST'))
 def service(name):
     image_url = url_for('static', filename=f'images/{name}.jpg')
-    
+    current_config=service_config[name]
+    current_ai_role=current_config["ai_role"]
     result = None
     if request.method == 'POST':
         prompt = request.form['title']
-        result = query_open_ai(prompt)
-        print(result)
+        result = query_open_ai(prompt, current_ai_role)
 
     return render_template("service.html", service_name=name, image_url=image_url, result=result)
 
---
+++ service_config.py
@@ -0,0 +1,14 @@
+service_config={
+    "trading" : {
+        "name": "trading",
+        "ai_role": "You are a trader answering only relevant questions to trading"
+        }, 
+    "realestate" : {
+        "name": "realestate",
+        "ai_role": "You are a real estate agent answering only relevant questions to real estate"
+        }, 
+    "dropshipping" : {
+        "name": "dropshipping",
+        "ai_role": "You are a dropshipper answering only relevant questions to dropshipping"
+        }
+}
